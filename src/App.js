b7265b6c00ad5e22c1faf47f15c398a2a2ff7ec1
hicoderindia <hicoder.dolly@gmail.com>
2022-01-02 13:18:48 +0530

    experiments

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
-import './App.scss';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <AccessAlarmIcon />
-        <Typography component="legend">Disabled</Typography>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is main page.
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
